bedbace04c71bc4317a2ef4ba33ce83b1c7848c4
Jeyamurugan Palaniselvam <jeyamurugan.p@sita.aero>
2018-06-06 17:26:43 +0530

    added comments

--- notes_2/debug.js
+++ notes_2/debug.js
@@ -3,6 +3,19 @@ var person = {
     age: 45
 };
 person.occupation = 'SDXSAAAX';
+
+debugger;
+
 person.age = 433;
 person.name = 'c';
 console.log(person.age);
+
+
+/*
+Run using node inspect-brk filename - chrom debugging
+Run using node inspect filename for commandline debug
+line(10)
+
+c
+
+*/
